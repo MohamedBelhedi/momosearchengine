c7a078c40376732a4fd63909b5ea46fff60f77cc
unknown <mbelhedi@outlook.com>
2022-05-13 20:13:52 +0200

    meine Suchmaschine heute gemacht 13.05.22

---
+++ function.js
@@ -0,0 +1,76 @@
+const input=document.querySelector("#search")
+const body=document.querySelector('body')
+
+const btn=document.querySelector('.btn')
+const color=document.querySelector('#color')
+const datumzeit=document.querySelector(".datumzeit")
+
+const date=new Date()
+
+datumzeit.innerText=date
+
+// const color1=color.value
+
+
+
+// const searchEnval=searchEn.value
+// const inputVal1='google'
+
+color.addEventListener("input",()=>{
+    body.style.background=color.value
+
+})
+
+
+const searchEn1=document.querySelector('.option1')
+const searchEn2=document.querySelector('.option2')
+const searchE3=document.querySelector('.option3')
+
+    
+
+    searchEn1.addEventListener("click",()=>{
+        const Input=input.value
+        console.log("hi")
+        
+        window.open("https://www.google.de/search?q="+`${Input}`)
+    
+input.value=''
+location.reload();
+})
+   
+
+searchEn2.addEventListener("click",()=>{
+        const Input=input.value
+        console.log("hi")
+        
+        window.open("https://www.bing.com/search?q="+`${Input}`)
+    
+input.value=''
+location.reload();
+})
+
+searchE3.addEventListener("click",()=>{
+        const Input=input.value
+        console.log("hi")
+        
+        window.open("https://de.search.yahoo.com/search?p"+`${Input}`)
+    
+input.value=''
+location.reload();
+})
+
+
+
+
+
+
+
+
+        
+     
+    
+    
+ 
+    
+
+
